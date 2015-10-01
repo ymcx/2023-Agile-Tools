7597e63ef8d9dbbee0a4169593cfb43c9fd065b7
Marta Cortés <mcortes@ee.oulu.fi>
2015-10-01 13:53:12 +0300

    Added function isPrime in module mathtools

---
+++ mathtools.py
@@ -0,0 +1,12 @@
+
+
+def isPrime(n):
+    '''Returns true if a number n is a prime number'''
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n < 2:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):   # only odd numbers
+        if n % i == 0:
+            return False
+    return True
