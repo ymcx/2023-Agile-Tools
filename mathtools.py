e9ab12300243ee43626da6a8b56f709800aad832
ymcx <consumers4@outlook.com>
2023-10-10 17:54:00 +0300

    committing fibonacci

--- mathtools.py
+++ mathtools.py
@@ -10,3 +10,12 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
+
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
