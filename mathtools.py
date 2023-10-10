d087cffddb4894893744a0a290999d8fcf1209ba
ymcx <consumers4@outlook.com>
2023-10-10 17:59:23 +0300

    committing arithmetics

--- mathtools.py
+++ mathtools.py
@@ -10,3 +10,12 @@ def isPrime(n):
         if n % i == 0:
             return False
     return True
+
+def arithmetic(a, difference, n):
+    '''Calculates the sum of a arithmetic serie of n elements.
+       An arithmetic sequence is of the form: a, a+d, a+2d, a+3d,...
+       n is the number of elements in the sequence.'''
+    #Get the arithmetic sequence
+    sequence = [a+difference*x for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
